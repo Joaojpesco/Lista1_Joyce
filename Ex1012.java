221bcb60f96877602fd57b3dc1c767f49fcfaead
joaojpesco369 <59544656+joaojpesco369@users.noreply.github.com>
2020-04-08 17:32:55 -0300

    Lista 01 - Java

--- Ex1012.java
+++ Ex1012.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Ex1012 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-
 		System.out.print("Informe o valor de A: ");
 		double a = scan.nextDouble();
 
