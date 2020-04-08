0dcfcfda7428d511e1d7079e5439952bcbc38d4a
joaojpesco369 <59544656+joaojpesco369@users.noreply.github.com>
2020-04-08 17:28:19 -0300

    Lista 01 - Java

--- Ex1036.java
+++ Ex1036.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Ex1036 {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
-		
 		System.out.print("Digite o valor de A");
 		double a = scan.nextDouble();
 
