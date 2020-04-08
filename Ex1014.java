6607086fa41da7ff873b5e088fb8ef5f95f2eaf3
joaojpesco369 <59544656+joaojpesco369@users.noreply.github.com>
2020-04-08 17:33:40 -0300

    Lista 01 - Java

--- Ex1014.java
+++ Ex1014.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Ex1014 {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
-
 		System.out.print("Distancia percorrida: ");
 		double dist = scan.nextDouble();
 		
 		System.out.print("Combustivel gasto: ");
 		double comb = scan.nextDouble();
 
-		System.out.printf("\nConsumo médio = %.3f",dist/comb);
+		System.out.printf("\nConsumo mÃ©dio = %.3f",dist/comb);
 	}
 }
