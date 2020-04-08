26084a415975d2d18348b26422eed0ce58022894
joaojpesco369 <59544656+joaojpesco369@users.noreply.github.com>
2020-04-08 17:25:56 -0300

    Lista 01 - Java

--- Ex1007.java
+++ Ex1007.java
@@ -2,9 +2,7 @@ import java.util.Scanner;
 
 public class Ex1007 {
 	public static void main(String[] args){
-	
 	Scanner scan = new Scanner(System.in);	
-	
 	System.out.print("Informe o valor de A: ");
 	int a = scan.nextInt();
 
