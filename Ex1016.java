fda94eadb633c10c7fcba1601e291ba10ebc10c0
joaojpesco369 <59544656+joaojpesco369@users.noreply.github.com>
2020-04-08 16:02:34 -0300

    Add files via upload

---
+++ Ex1016.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Ex1016 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Informe a distancia: ");
+		int a = scan.nextInt();
+		
+		System.out.printf("%d minutos",a*2);
+	}
+
+}
