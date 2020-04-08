d3429c5c48e635ad146ec20b4a34199bd4dc4f1f
joaojpesco369 <59544656+joaojpesco369@users.noreply.github.com>
2020-04-08 17:32:44 -0300

    Lista 01 - Java

--- Ex1011.java
+++ Ex1011.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Ex1011 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-
 		System.out.print("Informe o raio da esfera: ");
 		int raio = scan.nextInt();
 
 		double resultado = ((4.0/3) * 3.14159 * (raio*raio*raio));
 
-		System.out.printf("A area da esfera é %.3f",resultado);
+		System.out.printf("A area da esfera Ã© %.3f",resultado);
 	}
 
 }
