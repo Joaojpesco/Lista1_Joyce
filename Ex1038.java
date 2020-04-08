7638cab93418100a07128a786a77d3a8257161a2
joaojpesco369 <59544656+joaojpesco369@users.noreply.github.com>
2020-04-08 17:27:35 -0300

    Lista 01 - Java

--- Ex1038.java
+++ Ex1038.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 public class Ex1038 {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
-		
-		System.out.printf("Código            Produto                   Valor\n");
+		System.out.printf("CÃ³digo            Produto                   Valor\n");
 		System.out.printf("  1            Cachorro quente              R$ 4.00\n");
 		System.out.printf("  2            X Salada                     R$ 4.50\n");
 		System.out.printf("  3            X Bacon                      R$ 5.00\n");
 		System.out.printf("  4            Torrada simples              R$ 2.00\n");
 		System.out.printf("  5            Refrigerante                 R$ 1.50\n");
 
-		System.out.print("Digite o código do produto: ");
+		System.out.print("Digite o cÃ³digo do produto: ");
 		int c = scan.nextInt();
 		System.out.print("Digite quantidade: ");
 		int quant = scan.nextInt();
