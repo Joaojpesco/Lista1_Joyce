71b080d94fc3264b6914410e4352035c2a187eee
joaojpesco369 <59544656+joaojpesco369@users.noreply.github.com>
2020-04-08 17:23:53 -0300

    Lista 01 - Java

--- Ex1002.java
+++ Ex1002.java
@@ -3,10 +3,8 @@ import java.util.Scanner;
 public class Ex1002 {
 	public static void main(String[] args){
 		Scanner scan = new Scanner (System.in);
-		
 		System.out.print("Digite o raio do circulo: ");
 		double raio = scan.nextInt();
-			
 		System.out.print("Area do circulo = " + (3.14159 * (raio*raio)));
 	}
 }
