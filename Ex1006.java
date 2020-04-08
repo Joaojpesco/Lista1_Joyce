bd3453b7614f9d9ac3773815d7f3e40104be8da6
joaojpesco369 <59544656+joaojpesco369@users.noreply.github.com>
2020-04-08 17:25:43 -0300

    Lista 01 - Java

--- Ex1006.java
+++ Ex1006.java
@@ -4,7 +4,6 @@ import java.text.DecimalFormat;
 public class Ex1006 {
 	public static void main(String[] args) {
 		DecimalFormat saida = new DecimalFormat("0.0");
-		
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Informe a primeira nota: ");
 		double n1 = scan.nextFloat();
@@ -17,6 +16,6 @@ public class Ex1006 {
 		
 		double media = (((n1*2) + (n2*3) + (n3*5))/10);
 
-		System.out.printf("Média = " + saida.format(media));
+		System.out.printf("MÃ©dia = " + saida.format(media));
 	}
 }
